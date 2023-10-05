93bab8060980c3710f6af0011e0aa4cf67c18ff5
florianvazelle <ponythugflorian@gmail.com>
2023-10-05 21:52:10 +0200

    build: add support for python versions from 3.10 to 3.12 (#10)
    * build: add support for 3.7 to 3.12 python version
    
    * chore(readme): update changelog
    
    * build: remove 3.7, 3.8 and 3.9 python version support
    
    * fix(scripts): better sed than support windows platform

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ def lint(session: nox.Session) -> None:
     session.run("pre-commit", "run", "-a")
 
 
-@nox.session
+@nox.session(python=["3.10", "3.11", "3.12"])
 def test(session: nox.Session) -> None:
     session.install("poetry")
     session.run("poetry", "install")
