3d21d7e638406d6d53bcd0f7271c1e0e5b261c82
florianvazelle <ponythugflorian@gmail.com>
2023-10-16 19:32:16 +0200

    feat(scripts): add a new command (#23)
    * feat(scripts): add a new command
    
    * chore(changelog): add entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ Inspired from [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 ## [Unreleased]
 ### Added
 - Add Dockerfile and Github action ([#24](https://github.com/MechanicalFlower/magic_combo/pull/24))
+- Add command to easily setup new project ([#23](https://github.com/MechanicalFlower/magic_combo/pull/23))
 ### Changed
 ### Deprecated
 ### Removed
--- magic_combo/playbooks.py
+++ magic_combo/playbooks.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from invoke.collection import Collection
 from invoke.context import Context
 from invoke.tasks import task
@@ -19,6 +21,9 @@ from .tasks import (
 
 @task(pre=[clean_combo, clean_godot, clean_plug])
 def clean(c: Context) -> None:
+    """
+    Clean combo, godot and plug caches.
+    """
     pass
 
 
@@ -26,42 +31,56 @@ def clean(c: Context) -> None:
     pre=[
         clean_godot,
         clean_plug,
+        install_godot,
+        install_templates,
         install_addons,
         import_resources,
         export_release_linux,
     ]
 )
 def build(c: Context) -> None:
+    """
+    Build godot game for Linux.
+    """
     pass
 
 
 @task(pre=[build, run_release])
 def run(c: Context) -> None:
-    pass
-
-
-@task(pre=[export_release_linux, export_release_mac, export_release_windows])
-def export_release_all(c: Context) -> None:
+    """
+    Build and run godot game for Linux.
+    """
     pass
 
 
 @task(
-    pre=[
-        clean,
-        install_godot,
-        install_templates,
-        install_addons,
-        import_resources,
-        export_release_all,
-    ]
+    help={
+        "platforms": "Comma separated list of platform to export",
+        "all": "Export to all platform",
+    },
+    optional=["platform", "all"],
 )
-def ci_build(c: Context) -> None:
-    pass
+def export(
+    c: Context, platforms: Optional[str] = None, all: Optional[bool] = None
+) -> None:
+    """
+    Release export for any platform.
+    """
+    if all:
+        platforms = "windows,linux,mac"
+
+    if platforms:
+        for platform in platforms.split(","):
+            if platform.strip().lower() == "linux":
+                export_release_linux(c)
+            elif platform.strip().lower() == "windows":
+                export_release_windows(c)
+            elif platform.strip().lower() == "mac":
+                export_release_mac(c)
 
 
 playbook_ns = Collection("playbook")
 playbook_ns.add_task(clean)
 playbook_ns.add_task(build)
 playbook_ns.add_task(run)
-playbook_ns.add_task(export_release_all)
-playbook_ns.add_task(ci_build)
+playbook_ns.add_task(export)
